e58e5bc7cd5c7da82e0beff8222879910f6f6b21
Hye Won Park <hyewonpark26@gmail.com>
2022-04-11 22:53:00 -0700

    sets up server.js

--- server.js
+++ server.js
@@ -1,3 +1,4 @@
+require("dotenv").config;
 const express = require('express')
 // import sequelize connection
 
@@ -9,4 +10,8 @@ app.use(express.urlencoded({ extended: true }))
 app.use(require('./routes'))
 
 // sync sequelize models to the database, then turn on the server
-app.listen(process.env.PORT || 3000)
+async function init() {
+  await require("./config/connection").sync();
+  app.listen(process.env.PORT || 3000)
+}
+
